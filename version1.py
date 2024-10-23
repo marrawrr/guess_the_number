9f274f71c55e464d4901fb317db54fa2d2b9c2c4
marrawrr <marteedglaude@gmail.com>
2024-10-23 08:32:17 -0400

    Update version1.py

--- version1.py
+++ version1.py
@@ -1,15 +1,12 @@
 import random
 
-# Generate a random number between 1 and 10
 random_number = random.randint(1, 10)
 
-# Ask the user for their guess
 user_guess = int(input("Guess a number between 1 and 10: "))
 
-# Compare the user's guess to the random number
 if user_guess == random_number:
     print("Congratulations! You've guessed correctly, you won!")
 elif user_guess < random_number:
-    print("Your guess is too low. You were incorrect.")
+    print("You were incorrect.")
 else:
-    print("Your guess is too high. You were incorrect.")
+    print("You were incorrect.")
